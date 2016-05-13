36127f989fc86a8bf87ef429d0c23a1db4998596
AdrianLeyva <leyva97@live.com.mx>
2016-05-13 08:28:09 -0500

    Actualizado

--- app/src/main/java/comprafacil/myapp/root/comprafacil/ValidarUsuarioActivity.java
+++ app/src/main/java/comprafacil/myapp/root/comprafacil/ValidarUsuarioActivity.java
@@ -3,11 +3,11 @@ package comprafacil.myapp.root.comprafacil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ModoUsuario extends AppCompatActivity {
+public class ValidarUsuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_modo_usuario);
+        setContentView(R.layout.activity_validar_usuario);
     }
 }
