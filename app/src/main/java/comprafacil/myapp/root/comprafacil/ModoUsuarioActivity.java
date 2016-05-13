5dd41f35396b3ac2ce0dfdcf68303842ec3e13f9
kevingamboa17 <kevin-gamboa1@hotmail.com>
2016-05-13 09:43:07 -0500

    Primeras 2 actividades

--- app/src/main/java/comprafacil/myapp/root/comprafacil/ModoUsuarioActivity.java
+++ app/src/main/java/comprafacil/myapp/root/comprafacil/ModoUsuarioActivity.java
@@ -1,7 +1,9 @@
 package comprafacil.myapp.root.comprafacil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ModoUsuarioActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class ModoUsuarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modo_usuario);
+    }
+
+    public void abrirModoAdministrador(View view){
+        Intent i = new Intent(this, ValidarUsuarioActivity.class);
+        startActivity(i);
+    }
+
+    public void abrirModoClientes(View view){
+    /*    Intent i = new Intent(this, Cliente.class);
+        startActivity(i);
+    */
     }
 }
---
+++ app/src/main/java/comprafacil/myapp/root/comprafacil/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package comprafacil.myapp.root.comprafacil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreenActivity.this, ModoUsuarioActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+}
