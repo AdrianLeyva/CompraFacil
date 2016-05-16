dfef971d45d40d6a1c920273d3aff58bf0783259
AdrianLeyva <leyva97@live.com.mx>
2016-05-16 10:25:22 -0500

    Nueva actualización

--- app/src/main/java/persistencia/EmpleadoBD.java
+++ app/src/main/java/persistencia/EmpleadoBD.java
@@ -13,10 +13,10 @@ import comprafacil.myapp.root.comprafacil.ValidarUsuarioActivity;
 /**
  * Created by root_user on 6/05/16.
  */
-public class EmpleadoBD extends AsyncTask<String,Void,ResultSet>{
+public class EmpleadoBD extends AsyncTask<Void,Void,Void>{
 
-    private String servidor = "jdbc:mysql://127.0.0.1:3306/Empleados";
-    private String usuario = "root";
+    private String servidor = "jdbc:mysql://localhost:3306/CompraFacil";
+    private String usuario = "teamprogra";
     private String clave = "";
     private String driver = "com.mysql.jdbc.Driver";
     private Connection conexion;
@@ -59,48 +59,54 @@ public class EmpleadoBD extends AsyncTask<String,Void,ResultSet>{
 
 
     @Override
-    protected void onPostExecute(ResultSet result) {
+    protected Void doInBackground(Void... params) {
+        try{
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(servidor,usuario,clave);
+
+            Statement st = con.createStatement();
+            String sql = "select * from Empleados";
+
+            final ResultSet rs = st.executeQuery(sql);
+
+        }
+        catch (Exception e){
+
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
 
         String vistaUsuario = vista.getUsuario();
         String vistaClave = vista.getClave();
 
         try {
-
-            while (result.next()) {
+            while (rs.next()) {
                 if (vistaUsuario == rs.getString("Usuario") && vistaClave == rs.getString("Clave")) {
+                    int flag = 1;
                     Toast toast = Toast.makeText(vista,"Validación correcta",Toast.LENGTH_SHORT);
                     toast.show();
-                    flag = 1;
+                    break;
                 }
             }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-    }
 
-    @Override
-    protected ResultSet doInBackground(String... params) {
-        try {
-            Connection conn;
-            Class.forName(driver);
-            conn = DriverManager.getConnection(servidor, usuario, clave);
-
-            Statement estado = conn.createStatement();
-            String peticion = "select * from Empleados";
-            ResultSet result = estado.executeQuery(peticion);
-            return result;
-
-        }
         catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            Toast toast = Toast.makeText(vista,"Ha habido un error para enlazar la base de datos",Toast.LENGTH_SHORT);
+            toast.show();
         }
-        return null;
 
+        Toast toast = Toast.makeText(vista,"Validación incorrecta",Toast.LENGTH_SHORT);
+        toast.show();
+        int flag = 0;
+
+        super.onPostExecute(result);
     }
+
     public int getFlag(){
         return flag;
     }
+
 }
