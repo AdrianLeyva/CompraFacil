af0693069f922bfa861e0120061d813c274d4e87
DanielBaas <danfer742@gmail.com>
2016-05-26 08:22:06 -0500

    Es la clase provisionalnventario Daniel

--- app/src/main/java/persistencia/ProvisionalInventario.java
+++ app/src/main/java/persistencia/ProvisionalInventario.java
@@ -17,10 +17,21 @@ public class ProvisionalInventario {
         listaProductos.add(new Producto("Bebidas", "Coca", 10, 23));
         listaProductos.add(new Producto("Bebidas", "Sprite", 10, 23));
         listaProductos.add(new Producto("Bebidas", "Manzana", 10, 23));
-        listaProductos.add(new Producto("Bebidas", "Te", 10, 23));
-        listaProductos.add(new Producto("Bebidas", "Del valle", 10, 23));
-        listaProductos.add(new Producto("Bebidas", "Fanta", 10, 23));
-        listaProductos.add(new Producto("Bebidas", "Fresca", 10, 23));
+        listaProductos.add(new Producto("Botanas", "Charritos", 10, 23));
+        listaProductos.add(new Producto("Botanas", "Chicharrones", 10, 23));
+        listaProductos.add(new Producto("Botanas", "Nachos", 10, 23));
+        listaProductos.add(new Producto("Dulces", "Rockaleta", 10, 23));
+        listaProductos.add(new Producto("Dulces", "Chicles", 10, 23));
+        listaProductos.add(new Producto("Dulces", "Paletas", 10, 23));
+        listaProductos.add(new Producto("Galletas", "Emperador", 10, 23));
+        listaProductos.add(new Producto("Galletas", "Conquistador", 10, 23));
+        listaProductos.add(new Producto("Galletas", "Soles", 10, 23));
+        listaProductos.add(new Producto("Galletas", "Canelitas", 10, 23));
+        listaProductos.add(new Producto("Sabritas", "Doritos", 10, 23));
+        listaProductos.add(new Producto("Sabritas", "Rancheritos", 10, 23));
+        listaProductos.add(new Producto("Postres", "Napolitano", 10, 23));
+        listaProductos.add(new Producto("Postres", "Pastel", 10, 23));
+        listaProductos.add(new Producto("Postres", "Helado", 10, 23));
     }
 
     public ArrayList<Producto> getListaProductos() {
